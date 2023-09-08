6143d548bd72b9de5fd36cbb6f2721f1e5c81003
diegocosta21 <diegocosta21@yahoo.com.br>
2023-09-08 16:29:42 -0300

    código comentado

--- teste1.R
+++ teste1.R
@@ -1,2 +1,12 @@
 #testando enviou para o github
-View(iris)
+#visualizando a base de dados
+View(iris) 
+
+#sumário da base de dados iris
+summary(iris)
+
+# plots da base de dados iris
+plot(iris)
+
+# estrutura da base de dados iris
+str(iris)
